04c8c55bc569a014436c0d40a386564d69d0d1bf
over9003 <gabrielvartolomeu@yahoo.com>
2021-02-24 00:32:04 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+
+document.addEventListener("keypress", function(event) {
+  makeSound(event.key);
+  buttonAnimation(event.key);
+});
+
+function buttonAnimation(currentKey) {
+  var activeButton = document.querySelector(`.${currentKey}`);
+  activeButton.classList.add("pressed")
+  setTimeout(function(){activeButton.classList.remove("pressed")}, 50);
+
+}
+
+function makeSound(key) {
+  switch (key) {
+    case "w":
+      new Audio("sounds/tom-1.mp3").play();
+      break;
+    case "a":
+      new Audio("sounds/tom-2.mp3").play();
+      break;
+    case "s":
+      new Audio("sounds/tom-3.mp3").play();
+      break;
+    case "d":
+      new Audio("sounds/tom-4.mp3").play();
+      break;
+    case "j":
+      new Audio("sounds/snare.mp3").play();
+      break;
+    case "k":
+      new Audio("sounds/crash.mp3").play();
+      break;
+    case "l":
+      new Audio("sounds/kick-bass.mp3").play();
+      break;
+    default:
+      console.log(key);
+  }
+}
